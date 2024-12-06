35b4a7f9f1c4b63b1a5297dcc717f177e937cc59
koskei-kipkoech <patrickwayy@gmail.com>
2024-12-06 16:25:57 +0300

    updates lib

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 20, 'M', 'dark brown','calm', 0),
+('Rowdy', 10, 'M', 'black','intense',1),
+('Tabitha', 6, 'F', 'dark brown','nice',1),
+('Sergent Brown', 19, 'M' ,'green','slimy',0),
+('Melissa',13 , 'F' ,'dark brown','goofy',1),
+('Grinch', 2, 'M' ,'black','grinchy',1),
+('Wendy', 6, 'F', 'blue','naive',1),
+(Null, 20, 'M', 'black','aggresive',0);
+
